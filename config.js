0ac0e3abc5404f5060d71ac035e447f27339908e
interzone83 <charlie.perera@gmail.com>
2026-06-24 10:33:18 +0100

    new deploy

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
  * ---------
  * Paste your deployed Apps Script Web App /exec URL here.
  */
-const API_URL = "https://script.google.com/macros/s/AKfycbwocaOZm6-A1YjF7do8mCrX0jf-9iRui9bY5Sugl_Qh38Z6933DY5k0bFJjl8H8b7LRZQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbz2fTunG5RKDfmCaVDnIjJSrItbVuKpOAe1XabnyewC0T4EWSxmuLnca6AJN3OZNRdr/exec";
